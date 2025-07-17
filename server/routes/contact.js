47e0cc655eebbe78b6269cf71575f09ccd5fe18f
yorgo335 <yorgonassar04@gmail.com>
2025-07-17 18:53:51 +0300

    idk

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -7,12 +7,8 @@ const router = express.Router();
 
 const folderPath = "server/contactus-submissions";
 
-if (!fs.existsSync(folderPath)) {
-  fs.mkdirSync(folderPath);
-}
-
 router.post('/', (req, res, next)=>{
-    
+
     const filePath = path.join(folderPath, `${req.body.name} ${new Date().toLocaleString().replaceAll("/", "-").replaceAll(":", "-")}.txt`);
 
     fs.writeFile(filePath, `Name: ${req.body.name}\nEmail: ${req.body.email}\nPhone: ${req.body.phonenumber ? req.body.phonenumber : ""}\nComment: ${req.body.comment}`, (err) => {
